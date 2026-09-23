a26002b4cc0f64cac9ff808befc20a681880e6a9
Hanningtone Okoth <holdstonez@gmail.com>
2026-09-23 14:21:49 +0300

    Add complaints and adjustment models

---
+++ src/data/complaints.js
@@ -0,0 +1,5 @@
+export const complaintTypes=["Food cold","Incorrect item","Late delivery","Quality issue","Other"];
+export const complaintStatuses=["Open","Investigating","Resolved","Closed"];
+export const resolutions=["Refund","Discount","Complimentary Item","Replacement","Apology / No compensation"];
+export const initialComplaints=[];
+export const initialAdjustments=[];
