79d0ecbb88c3a9b94609285e5d3f12d6256c049a
Hanningtone Okoth <holdstonez@gmail.com>
2026-09-23 14:06:39 +0300

    Add POS data and storage modules: src/data/workflows.js

---
+++ src/data/workflows.js
@@ -0,0 +1,18 @@
+export const orderChannels = ["Dine In", "Take Away", "Delivery"];
+
+export const orderStatuses = [
+  "Placed",
+  "In Prep",
+  "Ready",
+  "Served/Out for Delivery"
+];
+
+export const deliveryStatuses = ["Assigned", "Picked Up", "Delivered"];
+
+export const complaintTypes = [
+  "Food cold",
+  "Incorrect item",
+  "Late delivery",
+  "Quality issue",
+  "Other"
+];
